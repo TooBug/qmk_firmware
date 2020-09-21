e9b74997fc6ff7e8aa5dc31f4c5e5fef5605e9c6
TooBug <i@toobug.net>
2020-09-21 15:33:57 +0800

    改变RESET位置，防止误触

--- keyboards/dg/xtrm/keymaps/default/keymap.c
+++ keyboards/dg/xtrm/keymaps/default/keymap.c
@@ -21,13 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
     KC_CAPS, KC_INS,  KC_HOME, KC_PGUP, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT,          KC_TRNS,
     KC_TRNS,          KC_DEL,  KC_END,  KC_PGDN, KC_TRNS, KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,          KC_TRNS,          DG_IME,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    KC_TRNS, KC_TRNS,          KC_TRNS,          DG_IME,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
   KEYMAP(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_TRNS, DG_MCTL, DG_APPW, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BRID, KC_BRIU, KC_TRNS,          KC_TRNS,
     KC_TRNS,          KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,          KC_TRNS,          RESET,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+    KC_TRNS, KC_TRNS,          KC_TRNS,          DG_IME,           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET)
 
 };
 
