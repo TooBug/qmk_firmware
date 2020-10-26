2fea60347d95268ac324016341a2f699bbce5430
TooBug <i@toobug.net>
2020-10-26 10:31:49 +0800

    prototype改变RESET位置，防止误触

--- keyboards/dg/prototype/keymaps/default/keymap.c
+++ keyboards/dg/prototype/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BRID, KC_BRIU, KC_TRNS,          KC_TRNS,
     KC_TRNS,          KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,          KC_TRNS,          RESET,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+    KC_TRNS, KC_TRNS,          KC_TRNS,          DG_IME,            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET)
 
 };
 
